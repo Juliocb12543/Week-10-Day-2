7d3a68697d8d8ecfcd282b448390b78ffcbbeaef
Julio Barrientos <113930475+Juliocb12543@users.noreply.github.com>
2022-10-27 17:22:40 +0000

    made functions and used return

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
-from methods_help import method_help
-# from functions import function
+# from methods_help import method_help
+from functions import function, welcome, add_numbers, num_dividedFrom_add_numbers
 # from returnStuff import returnS
 
-method_help()
-# function()
+# method_help()
+function()
+welcome("Julio")
+added = add_numbers(4, 5)
+print(added)
+final = num_dividedFrom_add_numbers(64, added)
+print(final)
 # returnS()
